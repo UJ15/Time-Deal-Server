67945587778f354e537276fb3611754c79bc0283
UJ15 <dbwogml15@naver.com>
2023-03-03 01:47:25 +0900

    :tada: begin a project

---
+++ src/main/java/com/uj15/timedeal/TimeDealApplication.java
@@ -0,0 +1,13 @@
+package com.uj15.timedeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeDealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeDealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uj15/timedeal/TimeDealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uj15.timedeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeDealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
