0b1a90b7a57ffd29e349324c9dcbd12f173765ea
UJ15 <dbwogml15@naver.com>
2023-03-08 06:27:45 +0900

    :sparkles: Add BaseEntity

--- src/main/java/com/uj15/timedeal/TimeDealApplication.java
+++ src/main/java/com/uj15/timedeal/TimeDealApplication.java
@@ -2,12 +2,13 @@ package com.uj15.timedeal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class TimeDealApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TimeDealApplication.class, args);
 	}
-
 }
