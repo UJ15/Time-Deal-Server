25610fb58ac287e098163642266b75590843884f
UJ15 <dbwogml15@naver.com>
2023-03-11 07:01:46 +0900

    :sparkles: Add updateProduct method in ProductRestController class

--- src/main/java/com/uj15/timedeal/product/controller/ProductRestController.java
+++ src/main/java/com/uj15/timedeal/product/controller/ProductRestController.java
@@ -2,10 +2,14 @@ package com.uj15.timedeal.product.controller;
 
 import com.uj15.timedeal.common.auth.annotation.Authorization;
 import com.uj15.timedeal.product.controller.dto.ProductCreateRequest;
+import com.uj15.timedeal.product.controller.dto.ProductUpdateRequest;
 import com.uj15.timedeal.product.service.ProductService;
 import com.uj15.timedeal.user.Role;
 import jakarta.validation.Valid;
+import java.util.UUID;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +31,12 @@ public class ProductRestController {
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody @Valid ProductCreateRequest request) {
         productService.createProduct(request);
+    }
+
+    @Authorization(role = Role.ADMIN)
+    @PatchMapping(value = "{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateProduct(@PathVariable UUID id, @RequestBody @Valid ProductUpdateRequest request) {
+        productService.updateProduct(id, request);
     }
 }
