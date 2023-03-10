658d7784e706bc7857a28a660b404badb97f6399
UJ15 <dbwogml15@naver.com>
2023-03-11 05:06:35 +0900

    :sparkles: Create ProductCreateRequest class

---
+++ src/main/java/com/uj15/timedeal/product/controller/dto/ProductCreateRequest.java
@@ -0,0 +1,28 @@
+package com.uj15.timedeal.product.controller.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+@Getter
+@RequiredArgsConstructor
+public class ProductCreateRequest {
+
+    @NotBlank
+    @Length(max = 50)
+    private final String name;
+
+    @NotBlank
+    @Length(max = 500)
+    private final String description;
+
+    @Min(value = 0)
+    private final long price;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+    private final LocalDateTime dealTime;
+}
