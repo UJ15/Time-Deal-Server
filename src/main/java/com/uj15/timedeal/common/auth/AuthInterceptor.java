ea386f5ca9282b20e291b8010f5f9b5fbd295fcf
UJ15 <dbwogml15@naver.com>
2023-03-10 16:03:59 +0900

    :sparkles: Create AuthInterceptor class

---
+++ src/main/java/com/uj15/timedeal/common/auth/AuthInterceptor.java
@@ -0,0 +1,34 @@
+package com.uj15.timedeal.common.auth;
+
+import com.uj15.timedeal.auth.SessionConst;
+import com.uj15.timedeal.auth.UserPrincipal;
+import com.uj15.timedeal.user.Role;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.Arrays;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class AuthInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+
+        Authorization authorization = ((HandlerMethod) handler).getMethodAnnotation(Authorization.class);
+        if (authorization == null) {
+            return true;
+        }
+        HttpSession session = request.getSession();
+        UserPrincipal principal = (UserPrincipal) session.getAttribute(SessionConst.KEY.name());
+        Role role = principal.getRole();
+
+        if (!Arrays.asList(authorization.role()).contains(role)) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "UnAuthorize");
+            return false;
+        }
+
+        return true;
+    }
+}
