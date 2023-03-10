63ae98178295756eab23616f47f18b0814bf720c
UJ15 <dbwogml15@naver.com>
2023-03-10 16:03:46 +0900

    :sparkles: Create Authorization annotation

---
+++ src/main/java/com/uj15/timedeal/common/auth/Authorization.java
@@ -0,0 +1,13 @@
+package com.uj15.timedeal.common.auth;
+
+import com.uj15.timedeal.user.Role;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Authorization {
+    Role[] role();
+}
