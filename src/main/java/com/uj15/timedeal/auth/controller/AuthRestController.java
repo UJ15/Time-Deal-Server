e385aac7c0c8106c453a24f26500764364559fb5
UJ15 <dbwogml15@naver.com>
2023-03-10 11:57:43 +0900

    :sparkles: Create AuthRestController class with login method

---
+++ src/main/java/com/uj15/timedeal/auth/controller/AuthRestController.java
@@ -0,0 +1,34 @@
+package com.uj15.timedeal.auth.controller;
+
+import com.uj15.timedeal.auth.UserPrincipal;
+import com.uj15.timedeal.auth.service.AuthService;
+import com.uj15.timedeal.common.auth.SessionConst;
+import com.uj15.timedeal.auth.controller.dto.UserLoginRequest;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/auth")
+public class AuthRestController {
+
+    private final AuthService authService;
+    private final HttpSession httpSession;
+
+    public AuthRestController(AuthService authService, HttpSession httpSession) {
+        this.authService = authService;
+        this.httpSession = httpSession;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void login(@RequestBody @Valid UserLoginRequest request) {
+        UserPrincipal user = authService.login(request);
+        httpSession.setAttribute(SessionConst.LOGIN_MEMBER, user);
+    }
+}
