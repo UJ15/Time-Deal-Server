1cf5be9822417e7f91b5ac2505e04a4c76cf676f
UJ15 <dbwogml15@naver.com>
2023-03-10 11:37:56 +0900

    :sparkles: Add Getter, change Enum save type

--- src/main/java/com/uj15/timedeal/user/entity/User.java
+++ src/main/java/com/uj15/timedeal/user/entity/User.java
@@ -4,16 +4,19 @@ import com.uj15.timedeal.common.entity.BaseEntity;
 import com.uj15.timedeal.user.Role;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Builder
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseEntity {
@@ -27,7 +30,7 @@ public class User extends BaseEntity {
     @Column(length = 20)
     private String password;
 
-    @Enumerated
+    @Enumerated(EnumType.STRING)
     private Role role;
 
     public static User of(UUID id, String password, String username, Role role) {
