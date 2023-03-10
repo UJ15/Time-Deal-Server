18cd63d05d2375c677d77a9c8e58f3f7ab3b1b9f
UJ15 <dbwogml15@naver.com>
2023-03-10 11:56:13 +0900

    :sparkles: Add findByUsername method

--- src/main/java/com/uj15/timedeal/user/repository/UserRepository.java
+++ src/main/java/com/uj15/timedeal/user/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.uj15.timedeal.user.repository;
 
 import com.uj15.timedeal.user.entity.User;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
+    @Query("select u from User u where u.username = :username")
+    Optional<User> findByUsername(String username);
 }
