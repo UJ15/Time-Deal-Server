45ad0209bc3410fb9008acd4e3f2c45d3cfaace8
UJ15 <dbwogml15@naver.com>
2023-03-10 17:25:25 +0900

    :art: fix code style

--- src/main/java/com/uj15/timedeal/user/controller/dto/UserCreateRequest.java
+++ src/main/java/com/uj15/timedeal/user/controller/dto/UserCreateRequest.java
@@ -3,7 +3,6 @@ package com.uj15.timedeal.user.controller.dto;
 import com.uj15.timedeal.user.Role;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.Length;
