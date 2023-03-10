e1edbb2fa0a892134cc6cfbdbadd0377682b7214
UJ15 <dbwogml15@naver.com>
2023-03-10 16:04:21 +0900

    :sparkles: Create SessionConst class

---
+++ src/main/java/com/uj15/timedeal/auth/SessionConst.java
@@ -0,0 +1,11 @@
+package com.uj15.timedeal.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum SessionConst {
+    KEY("loginMember");
+
+    private final String key;
+}
