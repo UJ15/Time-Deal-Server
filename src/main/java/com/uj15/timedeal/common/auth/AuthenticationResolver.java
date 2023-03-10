a9ab97c6df4d37e5a36b95b7ab57fb80e86e5942
UJ15 <dbwogml15@naver.com>
2023-03-10 16:03:15 +0900

    :sparkles: Create Authentication Resolver annotation

---
+++ src/main/java/com/uj15/timedeal/common/auth/AuthenticationResolver.java
@@ -0,0 +1,27 @@
+package com.uj15.timedeal.common.auth;
+
+import com.uj15.timedeal.auth.SessionConst;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class AuthenticationResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(Authentication.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        HttpSession session = request.getSession();
+        return session.getAttribute(SessionConst.KEY.name());
+    }
+}
