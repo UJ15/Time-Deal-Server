ef6956c810587f0959ad21200c2be4d5f45b2d6d
UJ15 <dbwogml15@naver.com>
2023-03-11 11:26:19 +0900

    :sparkles: Create OrderService class with createOrder method

---
+++ src/main/java/com/uj15/timedeal/order/service/OrderService.java
@@ -0,0 +1,54 @@
+package com.uj15.timedeal.order.service;
+
+import com.uj15.timedeal.order.entity.Order;
+import com.uj15.timedeal.order.repository.OrderRepository;
+import com.uj15.timedeal.product.entity.Product;
+import com.uj15.timedeal.product.repository.ProductRepository;
+import com.uj15.timedeal.user.entity.User;
+import com.uj15.timedeal.user.repository.UserRepository;
+import java.util.UUID;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
+    private final UserRepository userRepository;
+
+    public OrderService(
+            OrderRepository orderRepository,
+            ProductRepository productRepository,
+            UserRepository userRepository
+    ) {
+        this.orderRepository = orderRepository;
+        this.productRepository = productRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public void createOrder(UUID productId, UUID userId) {
+        if (orderRepository.findByProductIdAndUserId(productId, userId)
+                .isPresent()) {
+            throw new IllegalArgumentException("order is already exist");
+        }
+
+        Order order = getOrder(productId, userId);
+
+        orderRepository.save(order);
+    }
+
+    private Order getOrder(UUID productId, UUID userId) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new IllegalArgumentException("not exist product"));
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("not exist user"));
+
+        return Order.builder()
+                .product(product)
+                .user(user)
+                .build();
+    }
+}
