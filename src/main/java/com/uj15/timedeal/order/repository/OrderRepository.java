ec0d3303ba1b55de3797bd63bb94b719820a66ae
UJ15 <dbwogml15@naver.com>
2023-03-11 11:25:26 +0900

    :sparkles: Create OrderRepository class with findByProductIdAndUserId method

---
+++ src/main/java/com/uj15/timedeal/order/repository/OrderRepository.java
@@ -0,0 +1,13 @@
+package com.uj15.timedeal.order.repository;
+
+import com.uj15.timedeal.order.entity.Order;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface OrderRepository extends JpaRepository<Order, UUID> {
+
+    @Query("select o from Order o where o.user.id = :userId and o.product.id = :productId")
+    Optional<Order> findByProductIdAndUserId(UUID productId, UUID userId);
+}
