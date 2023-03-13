839e2eb30393e6f7a42c279afc7036769e728f4a
UJ15 <dbwogml15@naver.com>
2023-03-13 19:49:56 +0900

    :sparkles: Add getUserOrderProducts method in OrderRestController class

--- src/main/java/com/uj15/timedeal/order/controller/OrderRestController.java
+++ src/main/java/com/uj15/timedeal/order/controller/OrderRestController.java
@@ -3,8 +3,10 @@ package com.uj15.timedeal.order.controller;
 import com.uj15.timedeal.auth.UserPrincipal;
 import com.uj15.timedeal.common.auth.annotation.Authentication;
 import com.uj15.timedeal.common.auth.annotation.Authorization;
+import com.uj15.timedeal.order.controller.dto.UserOrderProductResponse;
 import com.uj15.timedeal.order.service.OrderService;
 import com.uj15.timedeal.user.Role;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,5 +30,11 @@ public class OrderRestController {
     @ResponseStatus(HttpStatus.CREATED)
     public void createOrder(@PathVariable UUID id, @Authentication UserPrincipal userPrincipal) {
         orderService.createOrder(id, userPrincipal.getUserId());
+    }
+
+    @Authorization(role = Role.USER)
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserOrderProductResponse> getUserOrderProducts(@Authentication UserPrincipal userPrincipal) {
+        return orderService.getUserOrderProducts(userPrincipal.getUserId());
     }
 }
