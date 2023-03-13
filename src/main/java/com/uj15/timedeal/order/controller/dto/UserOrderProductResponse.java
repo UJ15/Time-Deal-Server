5ca9743bc2c5678c0e75705a030a2c977c10c9a8
UJ15 <dbwogml15@naver.com>
2023-03-13 19:47:13 +0900

    :sparkles: Create UserOrderProductResponse class

---
+++ src/main/java/com/uj15/timedeal/order/controller/dto/UserOrderProductResponse.java
@@ -0,0 +1,22 @@
+package com.uj15.timedeal.order.controller.dto;
+
+import com.uj15.timedeal.order.entity.Order;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserOrderProductResponse {
+
+    private final UUID orderId;
+    private final UUID productId;
+    private final String productName;
+
+    public static UserOrderProductResponse from(Order order) {
+        return new UserOrderProductResponse(
+                order.getId(),
+                order.getProductId(),
+                order.getProductName()
+        );
+    }
+}
